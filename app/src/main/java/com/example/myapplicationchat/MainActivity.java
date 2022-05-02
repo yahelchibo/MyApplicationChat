19f25fe351fc8a7afc19c267b742cca4aefa4425
Yahel_c <yahel@yossis-MacBook-Air.local>
2022-05-02 09:45:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationchat/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.myapplicationchat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int RC_SIGN_IN = 777;
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private GoogleSignInClient mGoogleSignInClient;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestIdToken("779620881660-a3chh1b2lfa653ddsdefefg6jvv8qfdi.apps.googleusercontent.com")
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        Button signIn = findViewById(R.id.button1);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult( Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Intent i = new Intent(MainActivity.this, ChatActivity.class);
+            i.putExtra("user",account);
+            startActivity(i);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/myapplicationchat/chatMessage.java
@@ -0,0 +1,18 @@
+package com.example.myapplicationchat;
+
+public class chatMessage {
+
+    public String userPhoto;
+    public String userName;
+    public String userID;
+    public String message;
+
+    public chatMessage(String userPhoto, String userName, String userID, String message) {
+        this.userPhoto = userPhoto;
+        this.userName = userName;
+        this.userID = userID;
+        this.message = message;
+    }
+
+
+}
